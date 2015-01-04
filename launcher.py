657ff33e2a85d41644eb0a30c528dd1ad1d91d49
Eric T Dawson <eric.t.dawson@gmail.com>
2015-01-04 12:13:27 -0600

    Made jobfile a required parameter

--- launcher.py
+++ launcher.py
@@ -6,7 +6,7 @@ from LaunChair import LaunChair
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "-infile", dest="infile", type=str, default=sys.stdin)
+    parser.add_argument("-i", "-infile", dest="infile", type=str,required=True, default=sys.stdin)
     parser.add_argument("-c", "-cores", dest="cores", type=int, default=1, required=False)
     return parser.parse_args()
 
