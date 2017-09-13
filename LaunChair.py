b2451ebaa658e851fcc301e1df4a477c31c8acf2
Eric T. Dawson <eric.t.dawson@gmail.com>
2017-09-13 15:08:03 -0400

    Add -n option to control number of cpus

--- LaunChair.py
+++ LaunChair.py
@@ -16,6 +16,7 @@ class LaunChair:
     def __init__(self):
         self.jobfile = None
         self.work = None
+        self.n_cpus = 0
         return
 
     def set_file(self, jobfile):
@@ -29,16 +30,20 @@ class LaunChair:
                 work.append(line)
         self.work = work
 
+    def set_n_cpus(self, n):
+        if n == 0:
+            self.n_cpus = mp.cpu_count()
+        else:
+            self.n_cpus = int(n)
+
     def run(self, cores_per_task):
-        n_cpus = mp.cpu_count()
-        if cores_per_task > n_cpus:
+        if cores_per_task > self.n_cpus:
             raise ValueError("The number of cores per task must be less than the number of CPUs.")
-        size = n_cpus / cores_per_task
+        size = self.n_cpus / cores_per_task
 
         p = mp.Pool(size)
         try:
             ret = p.map_async(func, self.work).get(100000)
-            p.wait()
         except KeyboardInterrupt:
             p.terminate()
             exit(1)
--- launcher.py
+++ launcher.py
@@ -8,6 +8,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "-infile", dest="infile", type=str,required=True, default=sys.stdin)
     parser.add_argument("-c", "-cores", dest="cores", type=int, default=1, required=False)
+    parser.add_argument("-n", "-ncpus", dest="ncpus", type = int, default=0, required=False)
     return parser.parse_args()
 
 
@@ -15,5 +16,6 @@ if __name__ == "__main__":
     args = parse_args()
     l = LaunChair()
     l.set_file(args.infile)
+    l.set_n_cpus(args.ncpus)
     l.run(args.cores)
 
